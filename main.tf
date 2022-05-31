90e75f6efa283a6163c5e9becd02193432e28692
Bhushan <morapost@gmail.com>
2022-05-31 11:38:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@ provider "aws" {
   region = var.region
 }
 
-module "s3-staticsite" {
-  #source  = "app.terraform.io/bhushanpost/s3-webapp/aws"
-  source  = "app.terraform.io/bhushanpost/s3-staticsite/aws"
+module "s3-webapp" {
+  source  = "app.terraform.io/bhushanpost/s3-webapp/aws"
+  #source  = "app.terraform.io/bhushanpost/s3-staticsite/aws"
   name        = var.name
   region = var.region
   prefix = var.prefix
